cf97507e2444b4ea276f950fa31eac075acd7c09
Pradeep Kumar Sahu <krpradeep891@gmail.com>
2023-10-01 16:01:23 +0530

    Cors added

--- backend/index.js
+++ backend/index.js
@@ -3,12 +3,21 @@ import { PORT , mongoDBURL } from './config.js'
 import mongoose from 'mongoose';
 import { Book } from './models/bookModel.js';
 import booksRoute from './routes/booksRoute.js';
+import cors from 'cors';
 
 const app = express();
 
 app.use(express.json())
 app.use('/books', booksRoute);
 
+//Middleware for handling cors policy
+app.use(
+    cors({
+        origin: 'http://localhost:3000',
+        methods: ['GET','POST','PUT','DELETE'],
+        allowedHeaders: ['Content-Type'],
+    })
+)
 
 app.get('/' , (request, response) => {
     console.log(request);
