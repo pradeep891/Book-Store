5bb2a7b2d34a69f17eb54286034cf6930e5f2e19
Pradeep Kumar Sahu <krpradeep891@gmail.com>
2023-10-06 18:35:38 +0530

    Added code for creating a new book and deleting a book

--- frontend/src/pages/CreateBooks.jsx
+++ frontend/src/pages/CreateBooks.jsx
@@ -1,13 +1,73 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
+import React, { useState, useEffect } from "react";
+import { useNavigate, useParams } from "react-router-dom";
+import axios from "axios";
 
 const CreateBooks = () => {
- 
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+  const navigate = useNavigate();
+
+  const [title, setTitle] = useState("");
+  const [author, setAuthor] = useState("");
+  const [publishYear, setPublishYear] = useState("");
+
+  const handleCreateBook = () => {
+    const data = {
+      title: title,
+      author: author,
+      publishYear: publishYear,
+    };
+
+    axios
+      .post(`http://localhost:5555/books`, data)
+      .then((response) => {
+        setLoading(false); // Set loading to false when data is received successfully
+        navigate("/");
+      })
+      .catch((error) => {
+        setError(error); // Set error state if the request fails
+        setLoading(false); // Set loading to false even if there is an error
+      });
+
+    if (loading) {
+      return <div>Loading...</div>;
+    }
+
+    if (error) {
+      return <div>Error occurred: {error.message}</div>;
+    }
+  };
+
   return (
     <div>
-    Create a new book (fill all the details)
-  </div>
-  )
-}
+      <h1>Add a new book</h1>
+      <div>
+        <strong>Title:</strong>
+        <input
+          type="text"
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+        />
+      </div>
+      <div>
+        <strong>Author:</strong>
+        <input
+          type="text"
+          value={author}
+          onChange={(e) => setAuthor(e.target.value)}
+        />
+      </div>
+      <div>
+        <strong>Publish Year:</strong>
+        <input
+          type="text"
+          value={publishYear}
+          onChange={(e) => setPublishYear(e.target.value)}
+        />
+      </div>
+      <button onClick={handleCreateBook}>Save</button>
+    </div>
+  );
+};
 
-export default CreateBooks
+export default CreateBooks;
--- frontend/src/pages/DeleteBook.jsx
+++ frontend/src/pages/DeleteBook.jsx
@@ -3,12 +3,39 @@ import { useNavigate, useParams } from 'react-router-dom';
 import axios from 'axios';
 
 const DeleteBook = () => {
-  const [loading, setLoading] = useState(false);
-  const navigate = useNavigate();
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
   const { id } = useParams();
+  const navigate = useNavigate();
+
+  const handleDeleteBook = () => {
+    axios
+    .delete(`http://localhost:5555/books/${id}`)
+      .then(() => {
+        setLoading(false); // Set loading to false when data is received successfully
+        navigate("/");
+      })
+      .catch((error) => {
+        setError(error); // Set error state if the request fails
+        setLoading(false); // Set loading to false even if there is an error
+      });
+  
+    if (loading) {
+      return <div>Loading...</div>;
+    }
+  
+    if (error) {
+      return <div>Error occurred: {error.message}</div>;
+    }
+  }
+ 
 
   return (
-    <div>DeleteBook</div>
+    <div>Are you sure want to delete? 
+      <button onClick={handleDeleteBook}>Yes</button>
+      <button onClick={() => navigate("/")}>No</button>
+    </div>
+    
   )
 }
 
